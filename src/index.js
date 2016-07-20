0a25bc4df007111d7181ea8942ff3a4ac7c00c16
Zach Dahl <z.schtauffen@gmail.com>
2016-07-19 21:34:08 -0500

    wupwup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import '~/main.styl'
-import style from '~/buttons.styl'
+import button from '~/button.styl'
 
 const app = document.getElementById('app')
 
 app.innerHTML = `
   <a href="javascript:void(0)">blip</a>  
-  <button class="${style.link}">blop</button>
+  <button class="${button.link}">blop</button>
 `
